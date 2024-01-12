14bec4f582cd7cc12b190cfb107fa009f293693f
YoshikaiTaiki <2201212@s.asojuku.ac.jp>
2024-01-12 11:01:31 +0900

    touroku

--- src/List.php
+++ src/List.php
@@ -1,5 +1,6 @@
 <?php require 'header.php'; ?>
 <?php require 'db-connect.php'; ?>
+<?php require 'menu.php'; ?>
 <form action="List.php" method="post">
 商品検索
 <input type="text" name="keyword">
@@ -8,7 +9,7 @@
 <hr>
 <?php
 echo '<table>';
-echo '<tr><th>ID</th><th>動物名</th></tr>';
+echo '<tr><th>ID</th><th>動物の名前</th><th>部位ID</th></tr>';
 $pdo = new PDO($connect, USER, PASS);
 if(isset($_POST['keyword'])) {
     $sql = $pdo->prepare('SELECT * FROM AnimalMeat WHERE name LIKE ?');
@@ -17,12 +18,13 @@ if(isset($_POST['keyword'])) {
     $sql = $pdo->query('SELECT * FROM AnimalMeat'); // 修正: 'select * form product' を 'SELECT * FROM product' に修正
 }
 foreach($sql as $row){
-    $id = $row['animal_id'];
+    $id = $row['ID'];
     echo '<tr>';
     echo '<td>', $id, '</td>';
     echo '<td>';
-    echo '<a href="detail.php?id=', $id, '">', $row['Meat_name'], '</a>';
+    echo '<a href="detail.php?id=', $id, '">', $row['AnimalName'], '</a>';
     echo '</td>';
+    echo '<td>', $row['MeatPartID'], '</td>';
     echo '</tr>';
 }
 echo '</table>';
